dc6ad68eec2999c463fb8282211dbbf1b33f13d4
TimotejFasiang <60715817+TimotejFasiang@users.noreply.github.com>
2022-01-08 22:47:25 +0100

    Create VanocniUloha.java

---
+++ VanocniUloha.java
@@ -0,0 +1,66 @@
+// @author Timotej
+
+package vanocni.uloha;
+
+import java.util.*;
+
+public class VanocniUloha {
+    public static void main(String[] args) {
+        // Good luck Tim!
+		display_tree();
+		System.out.println("");
+		System.out.println("");
+    }
+
+	public static final String TEXT_RESET = "\u001B[0m";
+	public static final String TEXT_RED = "\u001B[31m";
+	public static final String TEXT_GREEN = "\u001B[32m";
+	public static final String TEXT_YELLOW = "\u001B[33m";
+	public static final String TEXT_BLUE = "\u001B[34m";
+	
+	static void empty(int sizeb) {
+		System.out.println("");
+		for (int i=0; i<sizeb; i++) {
+			System.out.print(" ");
+		}
+	}
+
+	static void full(int s, String ornament) {
+		for (int i=0; i<s; i++) {
+			double ran = Math.random()*100;
+			double rando = Math.random()*3;
+			if (ran < 85 ) {
+				System.out.print(TEXT_GREEN + "*" + TEXT_RESET);
+			}
+			else {
+				if (rando<1) {
+					System.out.print(TEXT_RED + ornament + TEXT_RESET);
+				}
+				else if (rando<2) {
+					System.out.print(TEXT_YELLOW + ornament + TEXT_RESET);
+				}
+				else {
+					System.out.print(TEXT_BLUE + ornament + TEXT_RESET);
+				}
+			}
+		}
+//		System.out.println("");
+	}
+	
+	static void display_tree() {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Výška vánočního stromku: ");
+		int size = sc.nextInt(); 
+		System.out.println("Typ ornamentu(znak): ");
+		String ornament = sc.next();
+		
+		int rows = size;
+
+		for (int r=1; r <= size; r++) {
+			int s = 2*r-1;
+			empty((2*size-2*r)/2);
+			full(s, ornament); 
+		}
+	}
+}
